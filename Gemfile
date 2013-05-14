972114c8ef208af0417bb082fe99c29d9eec644c
David Lee <ambitiouspear@gmail.com>
2013-05-14 00:39:58 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+gem 'strong_parameters', '0.2.1'
+gem 'jquery-rails'
+gem 'twitter-bootstrap-rails'
+gem 'devise', '~> 2.2.4'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripblink::Application.config.secret_token = '894538fb2a224c52c033c25e7a8e63da2fab15288ff85503bd055e6db712d25f40d75cc46365c08b6b206430ad29ea318af0d2209c440202f0ddc306ff85b187'

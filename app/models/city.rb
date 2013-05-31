4e0366311e2795786fe2acb5dbab9656decc7b74
David Lee <ambitiouspear@gmail.com>
2013-05-31 13:39:34 -0700

    clean up searchbar and edit readme.md

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ActiveRecord::Base
-  belongs_to :country
+  belongs_to :country, inverse_of: :cities
   has_attached_file :image, styles: { small: '270x270' }
 
   validates :name, presence: true, uniqueness: true
